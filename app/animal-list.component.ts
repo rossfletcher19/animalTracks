8a7446cead0bcbfab1dd3c85e96f93ea39f8c490
Ross Fletcher <rtbfletch@outlook.com>
2018-02-23 16:33:44 -0800

    add scrolling list and further style front end

--- app/animal-list.component.ts
+++ app/animal-list.component.ts
@@ -11,19 +11,20 @@ import { Animal } from './animal.model';
         <option value="animals2AndOlder">Animals 2 years and older</option>
         <option value="animalsYoungerThan2" >Animals Younger Than 2 years</option>
       </select>
-
-      <ul>
-       <li *ngFor="let currentAnimal of childAnimalList | animalview:filterByAnimalAge">
-                                                 <b>Name:</b> {{currentAnimal.name}} <br>
-                                                 <b>Species:</b> {{currentAnimal.species}} <br>
-                                                 <b>Age:</b> {{currentAnimal.age}} <br>
-                                                 <b>Diet:</b> {{currentAnimal.diet}} <br>
-                                                 <b>Zoo Location:</b> {{currentAnimal.zooLocation}} <br>
-                                                 <b># of Caretakers:</b> {{currentAnimal.numberOfCaretakers}} <br>
-                                                 <b>Sex:</b> {{currentAnimal.sex}} <br>
-                                                 <b>Likes:</b> {{currentAnimal.likes}} <br>
-                                                 <b>Dislikes:</b> {{currentAnimal.dislikes}} <br> <button (click)="editButtonHasBeenClicked(currentAnimal)">Edit!</button></li>
-     </ul>
+      <nav>
+        <ul>
+         <li *ngFor="let currentAnimal of childAnimalList | animalview:filterByAnimalAge">
+                                                   <b>Name:</b> {{currentAnimal.name}} <br>
+                                                   <b>Species:</b> {{currentAnimal.species}} <br>
+                                                   <b>Age:</b> {{currentAnimal.age}} <br>
+                                                   <b>Diet:</b> {{currentAnimal.diet}} <br>
+                                                   <b>Zoo Location:</b> {{currentAnimal.zooLocation}} <br>
+                                                   <b># of Caretakers:</b> {{currentAnimal.numberOfCaretakers}} <br>
+                                                   <b>Sex:</b> {{currentAnimal.sex}} <br>
+                                                   <b>Likes:</b> {{currentAnimal.likes}} <br>
+                                                   <b>Dislikes:</b> {{currentAnimal.dislikes}} <br> <button (click)="editButtonHasBeenClicked(currentAnimal)">Edit!</button></li>
+       </ul>
+     </nav>
   `
 })
 
